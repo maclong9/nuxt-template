0dcef458dc9acb116359c6a0407057785c9b8e7b
Mac <maclong9@icloud.com>
2024-11-25 21:57:34 +0000

    feat(layout): 💄 update `HeroTitle` text color
    Signed-off-by: Mac <maclong9@icloud.com>

--- components/Landing/Hero.vue
+++ components/Landing/Hero.vue
@@ -4,9 +4,7 @@
     :ui="{ wrapper: 'py-0 md:py-0 lg:py-0' }"
   >
     <ULandingHero
-      :title="$t('landing.hero.title')"
       :description="$t('landing.hero.description')"
-      orientation="horizontal"
       :links="[{
         label: $t('landing.hero.links.getStarted'),
         icon: 'i-heroicons-rocket-launch',
@@ -19,23 +17,26 @@
         size: 'lg',
         to: '/about',
       }]"
-      :ui="{ wrapper: 'py-12 sm:py-14 md:py-16' }"
+      :title="$t('landing.hero.title')"
+      :ui="{ wrapper: 'py-12 sm:py-14 md:py-16', title: 'text-neutral-900 dark:text-neutral-100' }"
+      orientation="horizontal"
     >
       <template #headline>
         <UButton
           :label="$t('landing.hero.links.whatsNew')"
+          class="rounded-full"
           color="gray"
+          size="xs"
           to="https://github.com/maclong9/nuxt-template/releases"
           trailing-icon="i-heroicons-arrow-right"
-          size="xs"
-          class="rounded-full"
         />
       </template>
       <template #default>
-        <img
-          src="https://picsum.photos/640/360"
+        <NuxtImg
+          alt="Hero Image Placeholder"
           class="w-full rounded-md shadow-xl ring-1 ring-gray-300 dark:ring-gray-700"
-        >
+          src="https://picsum.photos/640/360"
+        />
       </template>
     </ULandingHero>
   </ULandingSection>
---
+++ error.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import type { NuxtError } from '#app'
+
+defineProps<{
+  error: NuxtError
+}>()
+</script>
+
+<template>
+  <div>
+    <LayoutHeader />
+    <UContainer>
+      <UMain>
+        <UPage>
+          <UPageError :error="error" />
+        </UPage>
+      </UMain>
+    </UContainer>
+    <LayoutFooter />
+  </div>
+</template>
